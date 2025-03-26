195417415da0e8cf6db571c9bc1273303856b709
constanzabassi <cdb66@pitt.edu>
2025-03-26 16:22:29 -0400

    update with configuration file

--- __init__.py
+++ __init__.py
@@ -3,4 +3,5 @@ from .DataHandlerDecoding import DataHandlerDecoding
 from .Plotter import Plotter
 from .AnalysisManagerEncoding import AnalysisManagerEncoding
 from .DecoderAnalyzer import DecoderAnalyzer
+from .DatasetConfig import DatasetConfig
 
